6643e9f54e973f0d08c2853429a203afeb7633e6
Noemi Leon <noemi.telematik213@gmail.com>
2022-05-12 22:59:05 -0500

    Ejercicios capitulo 02_04

--- Program.cs
+++ Program.cs
@@ -1,27 +1,21 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace Avanzado2_2
 {
     class Program
     {
-        private enum Moneda
+        [Obsolete]
+        [Description]
+        public class Peticion
         {
-            MXN,
-            EUR,
-            USD
+            internal int Puerto { get; set; }
+            internal string Metodo { get; set; }
+            internal string TipoDeContenido { get; set; }
         }
         static void Main()
         {
-            decimal ConvertirMoneda(decimal cantidad, Moneda inicial, Moneda final) =>
-                 (inicial, final) switch
-                 {
-                     (Moneda.EUR, Moneda.MXN) => (cantidad * 21.27m),
-                     (Moneda.MXN, Moneda.EUR) => (cantidad * 0.047m),
-                     _ => throw new Exception("Combinacion no definida")
-                 };
-
-            var total = ConvertirMoneda(500m, Moneda.MXN, Moneda.EUR);
-            Console.WriteLine("total : " + total.ToString());
+            
         }
     }
 }
