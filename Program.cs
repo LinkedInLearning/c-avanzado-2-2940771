f5012c697f3589ba26d3a8dc5d83f473dcd19ce5
Noemi Leon <noemi.telematik213@gmail.com>
2022-05-12 22:11:04 -0500

    Ejercicios capitulo 01_01

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Avanzado2_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var paisesUE = new List<string>() { "España", "Austria", "Italia", "Alemania", "Dinamarca", "Suecia", "Finlandia" };
+            var paisesUEL = new List<string>();
+            foreach(var p in paisesUE)
+            {
+                if (p.Length > 6)
+                    paisesUEL.Add(p);
+            }
+
+            var paisesUELLinq = paisesUE.Where(p => p.Length > 6);
+        }
+    }
+}
