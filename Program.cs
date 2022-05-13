95ba8117127e9127950635dc24b991ce3824ac1c
Noemi Leon <noemi.telematik213@gmail.com>
2022-05-12 22:56:58 -0500

    Ejercicios capitulo 02_02

--- Program.cs
+++ Program.cs
@@ -4,17 +4,28 @@ namespace Avanzado2_2
 {
     class Program
     {
+        public class Peticion
+        {
+            internal int Puerto { get; set; }
+            internal string Metodo { get; set; }
+            internal string TipoDeContenido { get; set; }
+        }
         static void Main()
         {
-            object miNombre = 22;
-            if (miNombre is string)
-                Console.WriteLine("El nombre {0} tiene {1} caracteres",
-                    (string)miNombre, ((string)miNombre).Length);
+            object miNombre = "Noemi";
 
-            if (miNombre is string mn)
-                Console.WriteLine("El nombre {0} tiene {1} caracteres",
-                    mn, mn.Length);
+            if (miNombre is string { Length: 5})
+                Console.WriteLine("El nombre {0} tiene 5 caracteres",
+                    (string)miNombre);
 
+            bool ProcesarPeticion(Peticion pet) => pet switch
+            {
+                { TipoDeContenido: "image/*", Metodo: "GET" } => true,
+                { Puerto: 8080, Metodo: string { Length: 2 } } => true,
+                { TipoDeContenido: "image/*", Metodo: "POST" } when pet.Puerto.ToString().Length <= 4 => true,
+                { Metodo: "PUT", Puerto: 3000, TipoDeContenido: string ct } => ct.EndsWith("*"),
+                _ => false
+            };
         }
     }
 }
