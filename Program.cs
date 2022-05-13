0ee72c61d39f0e9ffb50acc347dea8d9538fceea
Noemi Leon <noemi.telematik213@gmail.com>
2022-05-12 23:13:34 -0500

    Ejercicios capitulo 04_02

--- Program.cs
+++ Program.cs
@@ -8,10 +8,30 @@ namespace Avanzado2_3
     {
         static void Main()
         {
+            //RUN
             var tr = Task.Run(() =>
             {
-               
+                Console.WriteLine("Tarea {0} se ejecuta en el hilo {1}",
+                    Task.CurrentId, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(2000);
             });
+            if (!tr.IsCompleted)
+                tr.Wait();
+
+            bool iniciarTarea = true;
+            //Start
+            var ts = new Task(() =>
+            {
+                Console.WriteLine("Tarea {0} se ejecuta en el hilo {1}",
+                    Task.CurrentId, Thread.CurrentThread.ManagedThreadId);
+            });
+            if (iniciarTarea)
+                ts.Start();
+
+            //verificar estado
+            if (ts.Status == TaskStatus.Running || ts.Status == TaskStatus.WaitingToRun)
+                ts.Wait();
+
         }
     }
 }
