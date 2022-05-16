715f706eacb12174820c9787d63f9ded07fedf2f
Noemi Leon <noemi.telematik213@gmail.com>
2022-05-15 20:40:26 -0500

    Ejercicios capitulo 05_03

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Avanzado2_5
 {
@@ -6,8 +7,12 @@ namespace Avanzado2_5
     {
         static void Main()
         {
-            var total = Divide(5, 0);
+            var total = Divide(5, 2);
             Console.WriteLine(total);
+
+            GuardarResultado(total.ToString());
+
+
         }
         static double Divide (int divisor, int dividendo)
         {
@@ -24,6 +29,30 @@ namespace Avanzado2_5
                 Console.WriteLine(ex);
             }
             return res;
+        }
+        static void GuardarResultado(string resultado)
+        {
+            string ruta = @"C:\temporal55\resultadoB.txt";
+            try
+            {
+                using (StreamWriter sw = File.AppendText(ruta))
+                {
+                    sw.WriteLine(resultado + "\n");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error leyendo archivo {0}. Mensaje = {1}",
+                    ruta, e.Message);
+            }
+            finally
+            {
+                if (!File.Exists(ruta))
+                {
+                    using StreamWriter sw = File.CreateText(ruta);
+                    File.AppendText($"Ha ocurrido un error, en {DateTime.Now}")
+                }
+            }
         }
 
     }
