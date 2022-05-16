cf652499d41e73804910c0e4ee8813c79fb602ca
Noemi Leon <noemi.telematik213@gmail.com>
2022-05-15 20:37:01 -0500

    Ejercicios capitulo 04_05

--- Program.cs
+++ Program.cs
@@ -17,8 +17,23 @@ namespace Avanzado2_3
             {
                 var msj = $"Tarea antecedente: {Task.CurrentId}";
                 Console.WriteLine(msj);
+                throw null;
                 return DateTime.Now;
             });
+            /*
+            try
+            {
+                ta.Wait();
+            }catch(AggregateException ae)
+            {
+                if (ae.InnerException is NullReferenceException)
+                    Console.WriteLine("excepción NULL");
+            }*/
+            ta.Wait();
+            _ = ta.IsCanceled;
+            if (ta.IsFaulted)
+                Console.WriteLine(ta.Exception);
+
 
             await ta.ContinueWith(a =>
             {
